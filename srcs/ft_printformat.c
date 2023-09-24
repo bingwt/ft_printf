d9f76b49084503d595b86aa0424e607c8eb6b1da
Bing Wen Tan <btan@c1r7s8.42singapore.sg>
2023-09-24 16:53:30 +0800

    modified:   srcs/ft_printformat.c

--- srcs/ft_printformat.c
+++ srcs/ft_printformat.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42.singapore.sg>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 11:39:19 by btan              #+#    #+#             */
-/*   Updated: 2023/09/24 15:50:34 by btan             ###   ########.fr       */
+/*   Updated: 2023/09/24 16:52:50 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,25 @@
 
 int	ft_printformat(int fmt, va_list lst)
 {
+	int	count;
+
+	count = 0;
 	if (fmt == 'c')
-		return (ft_printchar(va_arg(lst, int)));
+		count += ft_printchar(va_arg(lst, int));
 	if (fmt == 's')
-		return (ft_printstr(va_arg(lst, char *)));
-	if (fmt == 'p' || fmt == 'x')
-		return (ft_printhex((unsigned long long) va_arg(lst, void *), "0123456789abcdef"));
+		count += ft_printstr(va_arg(lst, char *));
+	if (fmt == 'p')
+	{
+		count += ft_printstr("0x");
+		count += ft_printhex((unsigned long long) va_arg(lst, void *), "0123456789abcdef");
+	}
 	if (fmt == 'd' || fmt == 'i')
-		return (ft_printnbr(va_arg(lst, int)));
+		count += ft_printnbr(va_arg(lst, int));
+	if (fmt == 'x')
+		count += ft_printhex(va_arg(lst, int), "0123456789abcdef");
 	if (fmt == 'X')
-		return (ft_printhex(va_arg(lst, int), "0123456789ABCDEF"));
+		count += ft_printhex(va_arg(lst, int), "0123456789ABCDEF");
 	if (fmt == '%')
-		return (ft_printchar(fmt));
-	return (0);
+		count += ft_printchar(fmt);
+	return (count);
 }

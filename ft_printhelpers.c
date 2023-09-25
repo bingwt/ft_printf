d584936289e325ba4cacbfecba122ea2cd7d53fa
Bing Wen Tan <btan@student.42singapore.sg>
2023-09-25 21:51:42 +0800

    modified:   ft_printhelpers.c

--- ft_printhelpers.c
+++ ft_printhelpers.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:12:25 by btan              #+#    #+#             */
-/*   Updated: 2023/09/25 13:20:41 by btan             ###   ########.fr       */
+/*   Updated: 2023/09/25 21:22:14 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,43 @@ int	ft_printchar(char c)
 	return (1);
 }
 
-int	ft_printstr
+int	ft_printstr(char *str)
+{
+	int	count;
+
+	count = 0;
+	if (!str)
+		str = "(null)";
+	while (*str)
+		count += ft_printchar(*(str++));
+	return (count);
+}
+
+int	ft_printnbr(int nb)
+{
+	size_t nbr;
+	int	count;
+
+	count = 0;
+	if (nb < 0)
+	{
+		count += ft_printchar('-');
+		nb = -nb;
+	}
+	nbr = nb;
+	if (nbr > 9)
+		ft_printnbr(nbr / 10);
+	count += ft_printchar('0' + nbr % 10);
+	return (count);
+}
+
+int	ft_printunbr(unsigned int nb)
+{
+	int	count;
+	if (nb > 9)
+		ft_printunbr(nb / 10);
+	count += ft_printchar(nb % 10);
+	return (count);
+}
+
+int	ft_printhex(
